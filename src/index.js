18e68d8063b335101969a976dc74d294cab00578
Diyaz <diyaz.murfin1@gmail.com>
2023-09-08 22:40:26 +0600

    Reset last weather info on every new fetch.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,40 @@
 const API_KEY = "d5a75bd487d14f13c64eabbf664cb5ad";
 
-const weather_info = document.querySelector("#weather-info");
 const btn = document.getElementById("btn");
 const city_name = document.getElementById("city-input");
 
 async function getWeather(city_name) {
+    const weather_tags = document.getElementById("weather-info").children;
+    for (let i = 0; i < weather_tags.length; i++) {
+        weather_tags[i].innerHTML = "";
+    }
+
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}&units=metric`);
     
     if (response.ok) {
-        const weather_info = await response.json();
+        const weather_json = await response.json();
 
-        let weather = weather_info["weather"][0]["main"];
-        let temp = weather_info["main"]["temp"];
-        let feels_like = weather_info["main"]["feels_like"]; 
-        let humidity = weather_info["main"]["humidity"]; 
-        let wind_speed = weather_info["wind"]["speed"];
-        let clouds = weather_info["clouds"]["all"];
+        let weather = weather_json["weather"][0]["main"];
+        let temp = weather_json["main"]["temp"];
+        let feels_like = weather_json["main"]["feels_like"]; 
+        let humidity = weather_json["main"]["humidity"]; 
+        let wind_speed = weather_json["wind"]["speed"];
+        let clouds = weather_json["clouds"]["all"];
 
-        document.getElementById("weather").append(weather);
-        document.getElementById("temp").append(temp);
-        document.getElementById("feels-like").append(feels_like);
-        document.getElementById("humidity").append(humidity);
-        document.getElementById("wind-speed").append(wind_speed);
-        document.getElementById("clouds").append(clouds);
+        let rain;
+        if (!(rain = weather_json["rain"])) rain = "Rain: Isn't raining"; 
+        else rain = `Rain: ${weather_json["rain"]["1h"]} mm`;
+
+        document.getElementById("weather").append(`Weather: ${weather}`);
+        document.getElementById("temp").append(`Temperature: ${temp} °`);
+        document.getElementById("feels-like").append(`Feels like: ${feels_like} °`);
+        document.getElementById("humidity").append(`Humidity: ${humidity} %`);
+        document.getElementById("wind-speed").append(`Wind speed: ${wind_speed} m/s`);
+        document.getElementById("rain").append(rain);
+        document.getElementById("clouds").append(`Clouds: ${clouds} %`);
     }
     else {
-        return document.getElementById("error").innerHTML = "Error fetching";
+        document.getElementById("error").innerHTML = `Error fetching: ${response.status}`;
     }
 }
 
